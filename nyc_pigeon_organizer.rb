f639458d81586ebc0d0b24129859a7dcf5e9e299
Jonathan Riecke <jonathanriecke@gmail.com>
2020-01-21 01:42:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,8 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   final_pigeon_list = {}
 #Gather Pigeon names out of given array
-  pigeon_names = data. { |x| }
+  pigeon_names = data.reduce do |word|
+    if word
   # write your code here!
   binding.pry
 end
