23912768722940de363f03451c0dadf25e91561b
Jonathan Riecke <jonathanriecke@gmail.com>
2020-01-21 02:19:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,9 +3,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   final_pigeon_list = {}
 #Gather Pigeon names out of given array
-  pigeon_names = data.reduce({}) do |result, element|
-    result[element.first.to_s] = element.last
-    result
+  data.each { |x, value|
   end
   # write your code here!
   binding.pry
